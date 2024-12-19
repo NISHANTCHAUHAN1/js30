43c68ea7f1e38a3cea8bb4045d2d241bc9d60470
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-12-18 22:40:09 -0500

    Add project files

---
+++ script.js
@@ -0,0 +1,316 @@
+const challenges = [
+  {
+    id: 1,
+    thumbnail: "thumbnail.png",
+    image: "Images/day1.png",
+    title: "Variables and Data Types",
+    day: "Day - 1",
+    solutionLink:
+      "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 2,
+    thumbnail: "thumbnail.png",
+    image: "Images/day2.png",
+    title: "Operators",
+    day: "Day - 2",
+    solutionLink:
+      "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 3,
+    thumbnail: "thumbnail.png",
+    image: "Images/day3.png",
+    title: "Control Structure",
+    day: "Day - 3",
+    solutionLink:
+      "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 4,
+    thumbnail: "thumbnail.png",
+    image: "Images/day4.png",
+    title: "Loops",
+    day: "Day - 4",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 5,
+    thumbnail: "thumbnail.png",
+    image: "Images/day5.png",
+    title: "Functions",
+    day: "Day - 5",
+    solutionLink:
+      "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 6,
+    thumbnail: "thumbnail.png",
+    image: "Images/day6.png",
+    title: "Arrays",
+    day: "Day - 6",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 7,
+    thumbnail: "thumbnail.png",
+    image: "Images/day7.png",
+    title: "Objects",
+    day: "Day - 7",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 8,
+    thumbnail: "thumbnail.png",
+    image: "Images/day8.png",
+    title: "ES6+ Features",
+    day: "Day - 8",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 9,
+    thumbnail: "thumbnail.png",
+    image: "Images/day9.png",
+    title: "DOM Manupulation",
+    day: "Day - 9",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 10,
+    thumbnail: "thumbnail.png",
+    image: "Images/day10.png",
+    title: "Event Handling",
+    day: "Day - 10",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 11,
+    thumbnail: "thumbnail.png",
+    image: "Images/day11.png",
+    title: "Promises and Async/Awaits",
+    day: "Day - 11",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 12,
+    thumbnail: "thumbnail.png",
+    image: "Images/day12.png",
+    title: "Error Handling",
+    day: "Day - 12",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 13,
+    thumbnail: "thumbnail.png",
+    image: "Images/day13.png",
+    title: "Modules",
+    day: "Day - 13",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 14,
+    thumbnail: "thumbnail.png",
+    image: "Images/day14.png",
+    title: "Classes",
+    day: "Day - 14",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 15,
+    thumbnail: "thumbnail.png",
+    image: "Images/day15.png",
+    title: "Closures",
+    day: "Day - 15",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 16,
+    thumbnail: "thumbnail.png",
+    image: "Images/day16.png",
+    title: "Recursion",
+    day: "Day - 16",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 17,
+    thumbnail: "thumbnail.png",
+    image: "Images/day17.png",
+    title: "Data Structures",
+    day: "Day - 17",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 18,
+    thumbnail: "thumbnail.png",
+    image: "Images/day18.png",
+    title: "Algorithms",
+    day: "Day - 18",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 19,
+    thumbnail: "thumbnail.png",
+    image: "Images/day19.png",
+    title: "Regular Expressions",
+    day: "Day - 19",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 20,
+    thumbnail: "thumbnail.png",
+    image: "Images/day20.png",
+    title: "LocalStorage and SessionStorage",
+    day: "Day - 20",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 21,
+    thumbnail: "thumbnail.png",
+    image: "Images/day21.png",
+    title: "LeetCode Easy",
+    day: "Day - 21",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 22,
+    thumbnail: "thumbnail.png",
+    image: "Images/day22.png",
+    title: "LeetCode Medium",
+    day: "Day - 22",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 23,
+    thumbnail: "thumbnail.png",
+    image: "Images/day23.png",
+    title: "LeetCode Hard",
+    day: "Day - 23",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 24,
+    thumbnail: "thumbnail.png",
+    image: "Images/day24.png",
+    title: "Project 1: Weather App",
+    day: "Day - 24",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 25,
+    thumbnail: "thumbnail.png",
+    image: "Images/day25.png",
+    title: "Project 2: Movies Search App",
+    day: "Day - 25",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 26,
+    thumbnail: "thumbnail.png",
+    image: "Images/day26.png",
+    title: "Project 3: Chat Application",
+    day: "Day - 26",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 27,
+    thumbnail: "thumbnail.png",
+    image: "Images/day27.png",
+    title: "Project 4: Task Management App",
+    day: "Day - 27",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 28,
+    thumbnail: "thumbnail.png",
+    image: "Images/day28.png",
+    title: "Project 5: Ecommerce Website",
+    day: "Day - 28",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 29,
+    thumbnail: "thumbnail.png",
+    image: "Images/day29.png",
+    title: "Project 6: Social Media Dashboard",
+    day: "Day - 29",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+  {
+    id: 30,
+    thumbnail: "thumbnail.png",
+    image: "Images/day30.png",
+    title: "Project 6: Social Media Dashboard",
+    day: "Day - 30",
+    solutionLink:
+    "https://youtu.be/Hr5iLG7sUa0?si=LHhVXvY0sbDt1EBM",
+  },
+];
+
+const challengesContainer = document.getElementById("challenges-container");
+const modal = document.getElementById("challenge-modal");
+const challengeImg = document.getElementById("challenge-img");
+const challengeTitle = document.getElementById("challenge-title");
+const challengeDescription = document.getElementById("challenge-description");
+const solveItBtn = document.getElementById("solve-it");
+
+challenges.forEach((challenge) => {
+  const card = document.createElement("div");
+  card.className = "challenge-card";
+  card.innerHTML = `
+        <img class="thumbnail" src="${challenge.thumbnail}" alt="${challenge.title}">
+        <h3>${challenge.title}</h3>
+        <p class = "day">${challenge.day}</p>
+        <button class="solve-it">View Solution</button>
+    `;
+  card.addEventListener("click", () => openChallenge(challenge));
+  challengesContainer.appendChild(card);
+});
+
+function openChallenge(challenge) {
+  challengeImg.src = challenge.image;
+  challengeTitle.textContent = challenge.title;
+  challengeDescription.textContent = challenge.description;
+  solveItBtn.onclick = () => window.open(challenge.solutionLink, "_blank");
+  modal.style.display = "block";
+}
+
+document.querySelector(".close-button").onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      modal.style.display = 'none';
+    }
+  });
